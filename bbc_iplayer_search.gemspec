mocoso/bbc_iplayer_search

Initial implementation

bbc_iplayer_search.gemspec
@@ -1,0 +1,20 @@
+Gem::Specification.new do |s|
+  s.name        = 'bbc_iplayer_search'
+  s.version     = '0.0.0'
+  s.date        = '2015-01-02'
+  s.summary     = "A simple search API for BBC iplayer"
+  s.description = "Built with some wonky page scraping"
+  s.authors     = ["Joel Chippindale"]
+  s.email       = 'joel@joelchippindale.com'
+  s.homepage    = 'https://github.com/mocoso/bbc_iplayer_search'
+  s.license = 'MIT'
+  s.files       = Dir['lib/**/*.rb'] + Dir['spec/**/*.rb'] +
+                    ['Rakefile', 'LICENSE.txt']
+
+  s.add_runtime_dependency 'nokogiri', '~> 1.6'
+  s.add_runtime_dependency 'httpclient', '~> 2.7'
+  s.add_development_dependency 'rake', '~> 10'
+  s.add_development_dependency 'rspec', '~> 3'
+  s.add_development_dependency 'webmock', '~> 1.22'
+  s.add_development_dependency 'vcr', '~> 3.0'
+end

lib/bbc_iplayer_search/search_results_page_not_recognised.rb
@@ -1,0 +1,4 @@
+module BBCIplayerSearch
+  class SearchResultsPageNotRecognised < StandardError
+  end
+end

mocoso/bfi_player_search

bbc_iplayer_search.gemspec
@@ -1,0 +1,20 @@
+Gem::Specification.new do |s|
+  s.name        = 'bfi_player_search'
+  s.version     = '0.0.0'
+  s.date        = '2015-01-02'
+  s.summary     = "A simple search API for BFI player"
+  s.description = "Built with some wonky page scraping"
+  s.authors     = ["Joel Chippindale"]
+  s.email       = 'joel@joelchippindale.com'
+  s.homepage    = 'https://github.com/mocoso/bfi_player_search'
+  s.license     = 'MIT'
+  s.files       = Dir['lib/**/*.rb'] + Dir['spec/**/*.rb'] +
+                    ['Rakefile', 'LICENSE.txt']
+
+  s.add_runtime_dependency 'nokogiri', '~> 1.6'
+  s.add_runtime_dependency 'httpclient', '~> 2.7'
+  s.add_development_dependency 'rake', '~> 10'
+  s.add_development_dependency 'rspec', '~> 3'
+  s.add_development_dependency 'webmock', '~> 1.22'
+  s.add_development_dependency 'vcr', '~> 3.0'
+end

lib/bfi_player_search/search_results_page_not_recognised.rb
@@ -1,0 +1,4 @@
+module BFIPlayerSearch
+  class SearchResultsPageNotRecognised < StandardError
+  end
+end